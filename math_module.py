0099c26c75c1d565b0cd00ff6a8b8608a2036d87
Deepa Dixit <95742534+Deepadixit98@users.noreply.github.com>
2022-01-10 10:18:17 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,39 @@
+# math Module
+import math
+
+# constants
+print(math.pi)
+print(math.e)
+
+# triginometric functions
+print(math.radians(180))
+print(math.radians(30))
+
+print(math.degrees(2 * math.pi))
+
+print(math.sin(math.radians(180)))
+print(math.sin(math.radians(30)))
+
+print(math.cos(math.radians(30)))
+
+print(math.tan(math.radians(30)))
+
+# logarithemic functions
+
+print(math.log(10))
+
+print(math.log10(100))
+
+print(math.exp(2))
+
+print(math.pow(2, 3))
+
+print(math.sqrt(225))
+
+# representation function
+
+print(math.ceil(12.5))
+
+print(math.floor(12.5))
+
+# end
